1fe2ef09be1ba81d07c4350ed41a4abace8fa059
Ruslan Bekshenev <rbekshenev23@gmail.com>
2022-07-30 03:59:32 +0500

    getServerSideProps

--- pages/contacts/[id].js
+++ pages/contacts/[id].js
@@ -2,6 +2,22 @@ import Heading from "../../components/Heading";
 import Head from "next/head";
 import ContactInfo from "../../components/ContactInfo";
 
+export const getServerSideProps = async (context) => {
+  const { id } = context.params
+  const response = await fetch(`http://jsonplaceholder.typicode.com/users/${id}`)
+  const data = await response.json()
+
+  if (!data) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: { contact: data }
+  }
+}
+
 const Contact = ({ contact }) => {
   return (
     <>
